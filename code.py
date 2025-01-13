c44cdee6e355b5402d863d02a1087c35f784c83f
Chandra4233 <shekar.reddy1415@gmail.com>
2025-01-13 19:21:23 +0530

    modified code includes function

--- code.py
+++ code.py
@@ -2,4 +2,15 @@ num = int(input('Enter the number : ' ))
 i = 1
 while i<=num:
     print(i)
-    i=i+1
+    i=i+1 # type: ignore
+
+def real_fun(): 
+    def inner_fun(s): 
+        return s+" how are you?" 
+    return inner_fun
+@real_fun
+def fun(s):
+    return s
+s="hii bro!"
+res = fun(s)
+print(res)
